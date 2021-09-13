5b055c344d24dc1929ef2f37c29a0de6904accf4
adeonita <adeonita.sousa@gmail.com>
2021-09-12 22:06:00 -0300

    feat: add client

--- src/index.php
+++ src/index.php
@@ -1,4 +1,9 @@
 <?php
-  require_once("./serverApplication.php");
+  // require_once("./serverApplication.php");
+  require_once("./view/index.php");
 
+  // $serverApplication = new Imc();
+  // $imc = $serverApplication->getClassificationIMC(60, 1.75);
+
+  // echo "<h1> $imc</h1>";
 ?>
