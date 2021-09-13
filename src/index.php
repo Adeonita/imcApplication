0ba601c08da5053fc3d9362da365451769912917
adeonita <adeonita.sousa@gmail.com>
2021-09-12 22:27:27 -0300

    chore: remove comments

--- src/index.php
+++ src/index.php
@@ -1,9 +1,3 @@
 <?php
-  // require_once("./serverApplication.php");
-  require_once("./view/index.php");
-
-  // $serverApplication = new Imc();
-  // $imc = $serverApplication->getClassificationIMC(60, 1.75);
-
-  // echo "<h1> $imc</h1>";
+  require_once("./client/clientApplication.php");
 ?>
