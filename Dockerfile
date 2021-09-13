934e9741606ae40d90e7647069ab3e870b4fbff7
adeonita <adeonita.sousa@gmail.com>
2021-09-12 21:15:02 -0300

    first commit

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,2 @@
 FROM php:7.2-apache
 RUN docker-php-ext-install mysqli
-
-COPY ./src/ /var/www/html/
--- src/index.php
+++ src/index.php
@@ -1,13 +1,4 @@
 <?php
+  require_once("./serverApplication.php");
 
-$mysqli = new mysqli("php_database_1", "root", "example");
-
-if ($mysqli->connect_errno) {
-    printf("Connect failed: %s\n", $mysqli->connect_error);
-    exit();
-}
-
-echo "successfully connected";
-
-$mysqli->close();
 ?>
